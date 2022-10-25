a79f9a7f0562929592a79584526724dd4691e8ad
Akshat Sharma <akshatsharma@Akshats-MacBook-Air.local>
2022-10-25 22:12:44 +0530

    added new question

---
+++ Tests/test_concatenation_of_array.py
@@ -0,0 +1,15 @@
+"""Test Solution for concatenation of array."""
+import logging
+
+import pytest
+
+from Solutions.concatenation_of_array import Solution
+
+mylogger = logging.getLogger()
+
+
+def test_first():
+    """First Test."""
+    mylogger.info("------------------> TEST START  <------------------")
+    mylogger.info("RUNNING FOR => CONCATENATION OF ARRAY ")
+    mylogger.info("-------------------> TEST END  <-------------------")
