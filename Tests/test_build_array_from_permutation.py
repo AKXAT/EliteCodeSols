96b9fa54e10ddbfd289f3ecc65ac72dd7d0966a6
Akshat Sharma <akshatsharma@Akshats-MacBook-Air.local>
2022-10-26 00:37:11 +0530

    added decorator for tests

--- Tests/test_build_array_from_permutation.py
+++ Tests/test_build_array_from_permutation.py
@@ -2,16 +2,16 @@
 import logging
 
 import pytest
+from assets import decorator
 
 from Solutions.build_array_from_permutation import Solution
 
 mylogger = logging.getLogger()
 
 
-def test_first():
+@decorator.testdecorator
+def test_build_array_from_permutation():
     """First Test."""
-    mylogger.info("------------------> TEST START  <------------------")
-    mylogger.info("RUNNING FOR => BUILD ARRAY FROM PERMUTATION ")
     input_array = [0, 2, 1, 5, 3, 4]
     result_array = [0, 1, 2, 4, 5, 3]
     mylogger.info(f"Checking for = {input_array}")
@@ -19,4 +19,3 @@ def test_first():
     mysolution = solution_object.buildArray(nums=input_array)
     mylogger.info(f"The result is = {mysolution}")
     assert mysolution == result_array, "Test Failed for Build Array from Permutation"
-    mylogger.info("-------------------> TEST END  <-------------------")
--- Tests/test_concatenation_of_array.py
+++ Tests/test_concatenation_of_array.py
@@ -1,15 +1,16 @@
 """Test Solution for concatenation of array."""
 import logging
+from importlib.abc import TraversableResources
 
 import pytest
+from assets import decorator
 
 from Solutions.concatenation_of_array import Solution
 
 mylogger = logging.getLogger()
 
 
-def test_first():
+@decorator.testdecorator
+def test_concatenation_of_array():
     """First Test."""
-    mylogger.info("------------------> TEST START  <------------------")
-    mylogger.info("RUNNING FOR => CONCATENATION OF ARRAY ")
-    mylogger.info("-------------------> TEST END  <-------------------")
+    assert True
